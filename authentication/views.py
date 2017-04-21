3aeecd6525bff4c1fdd607cecff774fe7ad725ce
Ilie Octavian <tavy14t@gmail.com>
2017-04-21 21:28:23 +0300

    little help with taking values from register form

--- authentication/views.py
+++ authentication/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from models import User
+import md5
 
 
 def login(request):
@@ -12,6 +13,14 @@ def register(request):
     if request.method == 'POST':
         form = User(request.POST)
         if form.is_valid():
+            firstname = form.cleaned_data['firstname']
+            lastname = form.cleaned_data['lastname']
+            mail = form.cleaned_data['mail']
+            password = form.cleaned_data['password']
+            md5_hasher = md5.new()
+            md5_hasher.update(password)
+            password = md5_hasher.digest()
+            # TODO : INSERT INTO DB OR PRINT ERROR IF ACCOUNT ALREADY EXISTS!
             return HttpResponseRedirect('/thanks/')
     else:
         form = User()
