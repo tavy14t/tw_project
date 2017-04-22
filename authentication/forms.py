f5a13bb12bc92699d6a6490d8260241d3590911c
Ilie Octavian <tavy14t@gmail.com>
2017-04-22 04:03:54 +0300

    implemented account registration using pl/sql

---
+++ authentication/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+# Create your models here.
+
+
+class UserForm(forms.Form):
+    firstname = forms.CharField(
+        label="First Name", max_length=64, required=True,
+        widget=forms.TextInput(attrs={'class': 'form-control',
+                                      'placeholder': 'Your First Name'}))
+
+    lastname = forms.CharField(label="Last Name", max_length=64, required=True,
+                               widget=forms.TextInput(
+                                   attrs={'class': 'form-control',
+                                          'placeholder': 'Your Last Name'}))
+
+    mail = forms.CharField(label="Mail Address", max_length=128, required=True,
+                           widget=forms.TextInput(
+                               attrs={'class': 'form-control',
+                                      'placeholder': 'Your Mail Address'}))
+
+    password = forms.CharField(
+        label="Password", max_length=128,
+        required=True,
+        widget=forms.PasswordInput(attrs={'class': 'form-control',
+                                          'placeholder': 'Password'}))
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.shortcuts import render_to_response
 from django.http import HttpResponseRedirect
-from models import User
+from django.contrib import messages
+from forms import UserForm
 import md5
+import cx_Oracle
+from trex import settings
 
 
 def login(request):
@@ -9,20 +13,63 @@ def login(request):
     })
 
 
+def create_account(form):
+    firstname = form.cleaned_data['firstname'].encode('utf8')
+    lastname = form.cleaned_data['lastname'].encode('utf8')
+    mail = form.cleaned_data['mail'].encode('utf8')
+    password = form.cleaned_data['password'].encode('utf8')
+    md5_hasher = md5.new()
+    md5_hasher.update(password)
+    password_hash = md5_hasher.hexdigest()
+
+    trex_db = settings.DATABASES['default']
+    ip = trex_db['HOST']
+    port = trex_db['PORT']
+    SID = trex_db['NAME']
+    user = trex_db['USER']
+    password = trex_db['PASSWORD']
+    dsn_tns = cx_Oracle.makedsn(ip, port, SID)
+
+    con = cx_Oracle.connect(user, password, dsn_tns)
+    cursor = con.cursor()
+    cursor.prepare('select * from users where email = :mail')
+    cursor.execute(None, {'mail': mail})
+    for line in cursor:
+        con.close()
+        return False
+
+    uid = int(cursor.callfunc('GET_UNUSED_ID', cx_Oracle.NUMBER, ['USERS']))
+
+    cursor.prepare(
+        'insert into users (USERID, FIRSTNAME, LASTNAME, '
+        'EMAIL, PASSWORDHASH, ISACTIVATED) '
+        'values (:userid, :fname, :lname, :email, :md5pass, 0)')
+
+    cursor.execute(None, {'userid': uid, 'fname': firstname,
+                          'lname': lastname, 'email': mail,
+                          'md5pass': password_hash})
+
+    cursor.prepare('select * from users where email = :mail')
+    cursor.execute(None, {'mail': mail})
+    for line in cursor:
+        con.close()
+        return True
+
+    return False
+
+
 def register(request):
     if request.method == 'POST':
-        form = User(request.POST)
+        form = UserForm(request.POST)
         if form.is_valid():
-            firstname = form.cleaned_data['firstname']
-            lastname = form.cleaned_data['lastname']
-            mail = form.cleaned_data['mail']
-            password = form.cleaned_data['password']
-            md5_hasher = md5.new()
-            md5_hasher.update(password)
-            password = md5_hasher.digest()
-            # TODO : INSERT INTO DB OR PRINT ERROR IF ACCOUNT ALREADY EXISTS!
-            return HttpResponseRedirect('/thanks/')
+            if not create_account(form):
+                messages.error(request, 'Account already exists!')
+            else:
+                messages.info(request, 'Account has been created!')
+
+            return render(request, 'registration/register.html',
+                          {'form': form})
     else:
-        form = User()
+        form = UserForm()
 
     return render(request, 'registration/register.html', {'form': form})
