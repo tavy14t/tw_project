e6b2353cc091a0d0646dee94a413e9d24c02a6ee
grosualex <grosu.alex1995@gmail.com>
2017-04-27 17:39:30 +0300

    Mici modificari

--- home/views.py
+++ home/views.py
@@ -10,14 +10,7 @@ from authentication.login_decorator import custom_login_required
 
 @custom_login_required
 def home_view(request):
-    result = Users.objects.all()
     context = dict()
-    context['list'] = []
-
-    print result
-
-    for item in result:
-        context['list'].append(item.firstname)
 
     context.update(commonviews.side_menu('Home'))
     return render(request, 'home/home.html', context)
