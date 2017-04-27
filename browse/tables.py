695520f823ea93ffe1225a5536d419dd22b99532
grosualex <grosu.alex1995@gmail.com>
2017-04-27 22:52:12 +0300

    Pregatim modulul browse.

---
+++ browse/tables.py
@@ -0,0 +1,2 @@
+import django_tables2 as tables
+from django.utils.safestring import mark_safe
--- home/views.py
+++ home/views.py
@@ -3,7 +3,6 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
-from models import Users
 from common import commonviews
 from authentication.login_decorator import custom_login_required
 
