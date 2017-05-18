a6d03d7b41eb2383c19f42f17da2410ab3d30019
Ionut Octavian ILIE <iilie@bitdefender.com>
2017-05-19 01:16:14 +0300

    Refactoring 2 (register + login = OK)

--- authentication/auth_controller.py
+++ authentication/auth_controller.py
@@ -1,7 +1,10 @@
 import re
 import hashlib
+import cx_Oracle
 from django.db import connection
 from enum import Enum
+
+mail_regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
 
 
 class AuthRC(Enum):  # Authentication Return Codes
@@ -9,7 +12,16 @@ class AuthRC(Enum):  # Authentication Return Codes
     EMAIL_TOO_LONG = 2,
     SUCCESS = 3,
     UNKNOWN_ERROR = 4,
-    ALREADY_LOGGED_IN = 5
+    ALREADY_LOGGED_IN = 5,
+
+
+class RegisterRC(Enum):
+    ALREADY_EXISTS = 1,
+    SUCCESS = 2,
+    INVALID_SQL_METHOD = 4,
+    INSERT_FAILED = 5,
+    INVALID_EMAIL_FORMAT = 6,
+    EMAIL_TOO_LONG = 7
 
 
 def get_hash(password):
@@ -22,10 +34,9 @@ def get_hash(password):
 def authenticate_user(request):
     email = request.POST['email'].encode('utf8')
     password = request.POST['password'].encode('utf8')
-    mail_regex = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
 
-    # if not re.match(mail_regex, request.POST['email']):
-    #     return AuthRC.INVALID_EMAIL
+    if not re.match(mail_regex, email):
+        return AuthRC.INVALID_EMAIL_FORMAT
 
     if len(email) >= 256:
         return AuthRC.MAIL_TOO_LONG
@@ -48,3 +59,56 @@ def authenticate_user(request):
     cursor.close()
 
     return AuthRC.UNKNOWN_ERROR
+
+
+def create_account(request):
+    firstname = request.POST['firstname'].encode('utf8')
+    lastname = request.POST['lastname'].encode('utf8')
+    email = request.POST['mail'].encode('utf8')
+    password = request.POST['password'].encode('utf8')
+    password_hash = get_hash(password)
+
+    print("[debug][create_account] firstname='{}'; "
+          "lastname='{}'; email='{}'; password='{}'; hash='{}'"
+          .format(firstname, lastname, email, password, password_hash))
+
+    if len(email) >= 256:
+        return RegisterRC.MAIL_TOO_LONG
+
+    if not re.match(mail_regex, email):
+        return RegisterRC.INVALID_EMAIL_FORMAT
+
+    cursor = connection.cursor()
+    cursor.execute('select * from users where email = :mail', {'mail': email})
+
+    for line in cursor:
+        print("[debug][create_account] User with mail='{}' "
+              " and id='{}' was not created!"
+              .format(email, line[0]))
+        return RegisterRC.ALREADY_EXISTS
+
+    try:
+        uid = int(cursor.callfunc('GET_UNUSED_ID',
+                                  cx_Oracle.NUMBER, ['USERS']))
+    except Exception:
+        return RegisterRC.INVALID_SQL_METHOD
+
+    cursor = connection.cursor()
+    cursor.execute("insert into users (USERID, FIRSTNAME, LASTNAME, "
+                   "EMAIL, PASSWORDHASH, ISACTIVATED, ROLE) values"
+                   "(:userid, :fname, :lname, :email, :md5pass, 0, 'U')",
+                   {'userid': uid, 'fname': firstname, 'lname':
+                    lastname, 'email': email, 'md5pass': password_hash})
+    cursor.close()
+
+    cursor = connection.cursor()
+    cursor.execute('select * from users where email = :mail', {'mail': email})
+
+    for line in cursor:
+        print("[debug][create_account] User with mail='{}' "
+              "and id='{}' was created!".format(email, uid))
+        return RegisterRC.SUCCESS
+
+    print("[debug][create_account] User with mail='{}' "
+          "and id='{}' was not created!".format(email, uid))
+    return RegisterRC.INSERT_FAILED
--- authentication/views.py
+++ authentication/views.py
@@ -10,26 +10,23 @@ import json
 from auth_controller import *
 
 
-def is_logged_in(request):
-    if 'userid' in request.session.keys():
-        return True
-    return False
-
-
 def login(request):
     if request.method == 'POST':
         auth_result = authenticate_user(request)
+
         if auth_result == AuthRC.INVALID_EMAIL_FORMAT:
             messages.error(request, 'Invalid email format!')
         elif auth_result == AuthRC.EMAIL_TOO_LONG:
             messages.error(request, 'Email is too long!')
         elif auth_result == AuthRC.SUCCESS:
-            messages.info(request, 'Login Succesfull!')
-        else:
+            messages.info(request, 'Login Successfully!')
+        elif auth_result == AuthRC.ALREADY_LOGGED_IN:
+            messages.info(request, 'Already logged in!')
+        elif auth_result == AuthRC.UNKNOWN_ERROR:
             messages.error(request, 'Unknown error has occured!')
         return render(request, 'login.html')
-
-    return render(request, 'login.html')
+    elif request.method == 'GET':
+        return render(request, 'login.html')
 
 
 def logout(request):
@@ -39,56 +36,24 @@ def logout(request):
     return HttpResponseRedirect('/login/')
 
 
-def create_account(form):
-    pass
-    # firstname = form.cleaned_data['firstname'].encode('utf8')
-    # lastname = form.cleaned_data['lastname'].encode('utf8')
-    # email = form.cleaned_data['mail'].encode('utf8')
-    # password = form.cleaned_data['password'].encode('utf8')
-    # password_hash = get_hash(password)
-    # print("[debug][create_account] firstname='{}'; lastname='{}'; email='{}'; password='{}'; hash='{}'"
-    #       .format(firstname, lastname, email, password, password_hash))
-
-    # cursor = connection.cursor()
-    # cursor.execute('select * from users where email = :mail', {'mail': email})
-
-    # for line in cursor:
-    #     print "[debug][create_account] User with mail='{}' and id='{}' was not created!".format(email, line[0])
-    #     return False
-
-    # uid = int(cursor.callfunc('GET_UNUSED_ID', cx_Oracle.NUMBER, ['USERS']))
-
-    # cursor = connection.cursor()
-    # cursor.execute("insert into users (USERID, FIRSTNAME, LASTNAME, EMAIL, PASSWORDHASH, ISACTIVATED, ROLE)"
-    #                "values (:userid, :fname, :lname, :email, :md5pass, 0, 'U')",
-    #                {'userid': uid, 'fname': firstname, 'lname': lastname, 'email': email, 'md5pass': password_hash})
-
-    # cursor.execute('select * from users where email = :mail', {'mail': email})
-
-    # for line in cursor:
-    #     print "[debug][create_account] User with mail='{}' and id='{}' was created!".format(email, uid)
-    #     return True
-
-    # print "[debug][create_account] User with mail='{}' and id='{}' was not created!".format(email, uid)
-    # return False
-
-
 def register(request):
-    pass
-    # if request.method == 'POST':
-    #     form = UserForm(request.POST)
-    #     if form.is_valid():
-    #         if not create_account(form):
-    #             messages.error(request, 'Account already exists!')
-    #         else:
-    #             messages.info(request, 'Account has been created!')
-
-    #         return render(request, 'registration/register.html',
-    #                       {'form': form})
-    # else:
-    #     form = UserForm()
-
-    # return render(request, 'registration/register.html', {'form': form})
+    if request.method == 'POST':
+        register_result = create_account(request)
+        if register_result == RegisterRC.ALREADY_EXISTS:
+            messages.error(request, 'Account already exists!')
+        elif register_result == RegisterRC.INVALID_SQL_METHOD:
+            messages.error(request, 'Internal server error!')
+        elif register_result == RegisterRC.INSERT_FAILED:
+            messages.error(request, 'Account creation failed!')
+        elif register_result == RegisterRC.SUCCESS:
+            messages.info(request, 'Account created succefully')
+        elif register_result == RegisterRC.INVALID_EMAIL_FORMAT:
+            messages.error(request, 'Invalid email format!')
+        elif register_result == RegisterRC.EMAIL_TOO_LONG:
+            messages.error(request, 'Invalid email format!')
+        return render(request, 'register.html')
+    elif request.method == 'GET':
+        return render(request, 'register.html')
 
 
 @custom_login_required
